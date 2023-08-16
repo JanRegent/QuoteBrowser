b1953ccf2eb4fd8185a9e2f233db912b0a9379b6
Kryštof Regent <1009@student.itgymnazium.cz>
2023-08-16 17:14:25 +0200

    AL---home-SidebarPage-2

--- lib/AL/_home/sidebar.dart
+++ lib/AL/_home/sidebar.dart
@@ -1,212 +1,241 @@
-import 'package:easy_sidemenu/easy_sidemenu.dart';
+import 'dart:math' as math show pi;
+
+import 'package:collapsible_sidebar/collapsible_sidebar.dart';
 import 'package:flutter/material.dart';
 
 class SidebarPage extends StatefulWidget {
   const SidebarPage({super.key});
+
   @override
   // ignore: library_private_types_in_public_api
   _SidebarPageState createState() => _SidebarPageState();
 }
 
 class _SidebarPageState extends State<SidebarPage> {
-  PageController pageController = PageController();
-  SideMenuController sideMenu = SideMenuController();
+  List<CollapsibleItem> _items = [];
+  String _headline = '';
 
   @override
   void initState() {
-    sideMenu.addListener((index) {
-      pageController.jumpToPage(index);
-    });
     super.initState();
+    _items = _generateItems;
+    _headline = _items.firstWhere((item) => item.isSelected).text;
+  }
+
+  List<CollapsibleItem> get _generateItems {
+    return [
+      CollapsibleItem(
+          text: 'Dashboard',
+          icon: Icons.assessment,
+          onPressed: () => setState(() => _headline = 'DashBoard'),
+          onHold: () => ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("Dashboard"))),
+          isSelected: true,
+          subItems: [
+            CollapsibleItem(
+              text: 'Menu',
+              icon: Icons.menu_book,
+              onPressed: () => setState(() => _headline = 'Menu'),
+              onHold: () => ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text("Menu"))),
+              isSelected: true,
+            ),
+            CollapsibleItem(
+                text: 'Shop',
+                icon: Icons.ac_unit,
+                onPressed: () => setState(() => _headline = 'Shop'),
+                onHold: () => ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text("Shop"))),
+                isSelected: true,
+                subItems: [
+                  CollapsibleItem(
+                    text: 'Cart',
+                    icon: Icons.shopping_cart,
+                    onPressed: () => setState(() => _headline = 'Cart'),
+                    onHold: () => ScaffoldMessenger.of(context)
+                        .showSnackBar(const SnackBar(content: Text("Cart"))),
+                    isSelected: true,
+                  )
+                ]),
+          ]),
+      CollapsibleItem(
+        text: 'Search',
+        icon: Icons.search,
+        onPressed: () => setState(() => _headline = 'Search'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Search"))),
+      ),
+      CollapsibleItem(
+        text: 'Notifications',
+        icon: Icons.notifications,
+        onPressed: () => setState(() => _headline = 'Notifications'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Notifications"))),
+      ),
+      CollapsibleItem(
+        text: 'Settings',
+        icon: Icons.settings,
+        onPressed: () => setState(() => _headline = 'Settings'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Settings"))),
+      ),
+      CollapsibleItem(
+        text: 'Alarm',
+        icon: Icons.access_alarm,
+        onPressed: () => setState(() => _headline = 'Alarm'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Alarm"))),
+      ),
+      CollapsibleItem(
+        text: 'Eco',
+        icon: Icons.eco,
+        onPressed: () => setState(() => _headline = 'Eco'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Eco"))),
+      ),
+      CollapsibleItem(
+        text: 'Event',
+        icon: Icons.event,
+        onPressed: () => setState(() => _headline = 'Event'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Event"))),
+      ),
+      CollapsibleItem(
+        text: 'No Icon',
+        onPressed: () => setState(() => _headline = 'No Icon'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("No Icon"))),
+      ),
+      CollapsibleItem(
+        text: 'Email',
+        icon: Icons.email,
+        onPressed: () => setState(() => _headline = 'Email'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Email"))),
+      ),
+      CollapsibleItem(
+          text: 'News',
+          onPressed: () => setState(() => _headline = 'News'),
+          onHold: () => ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("News"))),
+          subItems: [
+            CollapsibleItem(
+              text: 'Old News',
+              icon: Icons.elderly,
+              onPressed: () => setState(() => _headline = 'Old News'),
+              onHold: () => ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text("Old News"))),
+            ),
+            CollapsibleItem(
+                text: 'Current News',
+                icon: Icons.yard_outlined,
+                onPressed: () => setState(() => _headline = 'Current News'),
+                onHold: () => ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Current News"))),
+                subItems: [
+                  CollapsibleItem(
+                    text: 'News 1',
+                    icon: Icons.one_k,
+                    onPressed: () => setState(() => _headline = 'News 1'),
+                    onHold: () => ScaffoldMessenger.of(context)
+                        .showSnackBar(const SnackBar(content: Text("News 1"))),
+                  ),
+                  CollapsibleItem(
+                      text: 'News 2',
+                      icon: Icons.two_k,
+                      onPressed: () => setState(() => _headline = 'News 2'),
+                      onHold: () => ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("News 2"))),
+                      subItems: [
+                        CollapsibleItem(
+                          text: 'News 2 Detail',
+                          icon: Icons.two_k_outlined,
+                          onPressed: () =>
+                              setState(() => _headline = 'News 2 Detail'),
+                          onHold: () => ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                                  content: Text("News 2 Detail"))),
+                        )
+                      ]),
+                  CollapsibleItem(
+                    text: 'News 3',
+                    icon: Icons.three_k,
+                    onPressed: () => setState(() => _headline = 'News 3'),
+                    onHold: () => ScaffoldMessenger.of(context)
+                        .showSnackBar(const SnackBar(content: Text("News 3"))),
+                  )
+                ]),
+            CollapsibleItem(
+              text: 'New News',
+              icon: Icons.account_balance,
+              onPressed: () => setState(() => _headline = 'New News'),
+              onHold: () => ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text("New News"))),
+            ),
+          ]),
+      CollapsibleItem(
+        text: 'Face',
+        icon: Icons.face,
+        onPressed: () => setState(() => _headline = 'Face'),
+        onHold: () => ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("Face"))),
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SideMenu(
-            controller: sideMenu,
-            style: SideMenuStyle(
-              // showTooltip: false,
-              displayMode: SideMenuDisplayMode.auto,
-              hoverColor: Colors.blue[100],
-              selectedHoverColor: Colors.blue[100],
-              selectedColor: Colors.lightBlue,
-              selectedTitleTextStyle: const TextStyle(color: Colors.white),
-              selectedIconColor: Colors.white,
-              // decoration: BoxDecoration(
-              //   borderRadius: BorderRadius.all(Radius.circular(10)),
-              // ),
-              // backgroundColor: Colors.blueGrey[700]
-            ),
-            footer: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.lightBlue[100],
-                    borderRadius: BorderRadius.circular(12)),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
-                  child: Text(
-                    'mohada',
-                    style: TextStyle(fontSize: 15, color: Colors.grey[800]),
-                  ),
-                ),
-              ),
-            ),
-            items: [
-              SideMenuItem(
-                title: 'Dashboard',
-                onTap: (index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.home),
-                badgeContent: const Text(
-                  '3',
-                  style: TextStyle(color: Colors.white),
-                ),
-                tooltipContent: "This is a tooltip for Dashboard item",
-              ),
-              SideMenuItem(
-                title: 'Users',
-                onTap: (index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.supervisor_account),
-              ),
-              SideMenuItem(
-                title: 'Files',
-                onTap: (index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.file_copy_rounded),
-                trailing: Container(
-                    decoration: const BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.all(Radius.circular(6))),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 6.0, vertical: 3),
-                      child: Text(
-                        'New',
-                        style: TextStyle(fontSize: 11, color: Colors.grey[800]),
-                      ),
-                    )),
-              ),
-              SideMenuItem(
-                title: 'Download',
-                onTap: (index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.download),
-              ),
-              SideMenuItem(
-                builder: (context, displayMode) {
-                  return const Divider(
-                    endIndent: 8,
-                    indent: 8,
-                  );
-                },
-              ),
-              SideMenuItem(
-                title: 'Settings',
-                onTap: (index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.settings),
-              ),
-              // SideMenuItem(
-              //   onTap:(index, _){
-              //     sideMenu.changePage(index);
-              //   },
-              //   icon: const Icon(Icons.image_rounded),
-              // ),
-              // SideMenuItem(
-              //   title: 'Only Title',
-              //   onTap:(index, _){
-              //     sideMenu.changePage(index);
-              //   },
-              // ),
-              const SideMenuItem(
-                title: 'Exit',
-                icon: Icon(Icons.exit_to_app),
-              ),
-            ],
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: CollapsibleSidebar(
+        isCollapsed: MediaQuery.of(context).size.width <= 800,
+        items: _items,
+        collapseOnBodyTap: false,
+        body: _body(size, context),
+        backgroundColor: Colors.black,
+        selectedTextColor: Colors.limeAccent,
+        textStyle: const TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+        titleStyle: const TextStyle(
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold),
+        toggleTitleStyle:
+            const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        sidebarBoxShadow: const [
+          BoxShadow(
+            color: Colors.indigo,
+            blurRadius: 20,
+            spreadRadius: 0.01,
+            offset: Offset(3, 3),
           ),
-          Expanded(
-            child: PageView(
-              controller: pageController,
-              children: [
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Dashboard',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Users',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Files',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Download',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Settings',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Only Title',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const Center(
-                    child: Text(
-                      'Only Icon',
-                      style: TextStyle(fontSize: 35),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          BoxShadow(
+            color: Colors.green,
+            blurRadius: 50,
+            spreadRadius: 0.01,
+            offset: Offset(3, 3),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _body(Size size, BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.blueGrey[50],
+      child: Center(
+        child: Transform.rotate(
+          angle: math.pi / 2,
+          child: Transform.translate(
+            offset: Offset(-size.height * 0.3, -size.width * 0.23),
+            child: Text(
+              _headline,
+              overflow: TextOverflow.visible,
+              softWrap: false,
+            ),
+          ),
+        ),
       ),
     );
   }
