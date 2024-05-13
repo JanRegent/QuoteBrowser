7b9b67c58a7b0c8e1c73f403c6e4c0f2ce6d6db4
Kryštof Regent <1009@student.itgymnazium.cz>
2024-05-13 16:48:18 +0200

    AL--previewPage---text_with_highlight

--- lib/1PL/zresults/swiperbrowser/prewiew/previewpage.dart
+++ lib/1PL/zresults/swiperbrowser/prewiew/previewpage.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:highlight_text/highlight_text.dart';
 
 import '../../../../2BL_domain/bl.dart';
+import 'text_with_highlight.dart';
 
 bool previewPageOn = false;
 
@@ -12,30 +13,44 @@ class PreviewPage extends StatelessWidget {
   VoidCallback swiperSetstate;
   PreviewPage(this.swiperSetstate, {super.key});
 
+  Row titleRow() {
+    return Row(
+      children: [
+        Obx(() => Text(bl.orm.currentRow.author.value)),
+        const Text(' / '),
+        Obx(() => Text(
+            '${bl.orm.currentRow.book.value}\n${bl.orm.currentRow.parPage.value}'))
+      ],
+    );
+  }
+
+  IconButton previewPageReset() {
+    return IconButton(
+        onPressed: () {
+          previewPageOn = false;
+          swiperSetstate();
+        },
+        icon: const Icon(Icons.edit));
+  }
+
+  List<String> highlightedTexts = [];
+
+  void highStrings() {
+    highlightedTexts = [];
+
+    List<String> parts = bl.orm.currentRow.yellowParts.value.split('__|__');
+
+    for (var i = 0; i < parts.length; i++) {
+      if (parts[i].isEmpty) continue;
+      highlightedTexts.add(parts[i].trim());
+    }
+    List<String> tags = bl.orm.currentRow.tags.value.split('#');
+    highlightedTexts.addAll(tags);
+  }
+
   @override
   Widget build(BuildContext context) {
-    Map<String, HighlightedWord> words = {};
-    words = highParts();
-
-    Row titleRow() {
-      return Row(
-        children: [
-          Obx(() => Text(bl.orm.currentRow.author.value)),
-          const Text(' / '),
-          Obx(() => Text(
-              '${bl.orm.currentRow.book.value}\n${bl.orm.currentRow.parPage.value}'))
-        ],
-      );
-    }
-
-    IconButton previewPageReset() {
-      return IconButton(
-          onPressed: () {
-            previewPageOn = false;
-            swiperSetstate();
-          },
-          icon: const Icon(Icons.edit));
-    }
+    highStrings();
 
     return Scaffold(
         appBar: AppBar(
@@ -47,63 +62,30 @@ class PreviewPage extends StatelessWidget {
           actions: [previewPageReset()],
         ),
         body: SingleChildScrollView(
-            child: Obx(() => TextHighlight(
-                text: bl.orm.currentRow.quote
-                    .value, // You need to pass the string you want the highlights
-                words: words,
-                matchCase: false, //// Your dictionary words
-                textStyle: const TextStyle(fontSize: 20.0, color: Colors.black),
-                textAlign: TextAlign.left))));
+            child: Obx(
+          () => TextWithHighlight(
+            text: bl.orm.currentRow.quote.value,
+            textStyle: const TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+              color: Colors.black,
+            ),
+            highlightedTexts: highlightedTexts,
+            highlightedTextStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.red,
+            ),
+          ),
+
+          // TextHighlight(
+          //     text: bl.orm.currentRow.quote
+          //         .value, // You need to pass the string you want the highlights
+          //     words: words,
+          //     splitOnLongWord: true,
+          //     matchCase: false, //// Your dictionary words
+          //     textStyle: const TextStyle(fontSize: 20.0, color: Colors.black),
+          //     textAlign: TextAlign.left)
+        )));
   }
-}
-
-//----------------------------------------------------------------------
-Map<String, HighlightedWord> highTags() {
-  Map<String, HighlightedWord> words = {};
-
-  TextStyle tagStyle = const TextStyle(
-      fontSize: 20.0, color: Colors.red, fontWeight: FontWeight.bold);
-
-  List<String> tags = bl.orm.currentRow.tags.value.split('#');
-  for (var i = 0; i < tags.length; i++) {
-    if (tags[i].isEmpty) continue;
-
-    words.addAll({
-      tags[i]: HighlightedWord(
-        onTap: () {
-          debugPrint(tags[i]);
-        },
-        textStyle: tagStyle,
-      )
-    });
-  }
-
-  return words;
-}
-
-Map<String, HighlightedWord> highParts() {
-  Map<String, HighlightedWord> words = {};
-
-  TextStyle partStyle =
-      const TextStyle(fontSize: 20.0, backgroundColor: Colors.yellow);
-
-  List<String> parts = bl.orm.currentRow.yellowParts.value.split('__|__');
-
-  for (var i = 0; i < parts.length; i++) {
-    if (parts[i].isEmpty) continue;
-    List<String> partwords = parts[i].replaceAll(',', '').trim().split(' ');
-    for (var wix = 0; wix < partwords.length; wix++) {
-      if (partwords[wix].length <= 2) continue;
-      words.addAll({
-        partwords[wix]: HighlightedWord(
-          onTap: () {
-            debugPrint(partwords[wix]);
-          },
-          textStyle: partStyle,
-        )
-      });
-    }
-  }
-  words.addAll(highTags());
-  return words;
 }
--- lib/3Data/builddate.dart
+++ lib/3Data/builddate.dart
@@ -1,1 +1,1 @@
-String buildDate = ' 2024_05_13__13_28_build_AL--QuoteAddPage--- ';  
+String buildDate = ' 2024_05_13__16_48_build_AL--previewPage---text_with_highlight ';  
